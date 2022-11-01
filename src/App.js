dd11bf669f588bb2819a486964591a9338e35f2d
YusufLaidey <yusuflaideylee@gmail.com>
2022-11-01 11:29:25 +0100

    iterating over the data and rendering them dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import Card from "./components/Card";
+import CardsList from "./components/CardsList";
 import { robots } from "./components/robot";
-
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      <CardsList robots = { robots }/>
     </div>
   );
 }
